2fd56d819968d4623f7238f46f2b74de76b9f6e1
tulsitripathy <80155544+tulsitripathy@users.noreply.github.com>
2021-06-09 17:22:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,70 @@
+const Engine = Matter.Engine
+const World = Matter.World
+const Events = Matter.Events
+const Bodies = Matter.Bodies  
+
+var particles = [];
+var plinkos = [];
+var divisions = [];
+var divisionHeight = 300;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function setup() {
+  createCanvas(480,800);
+
+
+engine = Engine.create();
+ world = engine.world;
+ ground = new Ground(width/2, height, width, 20)
+ 
+ for (var k = 0; k <=width; k = k + 80){
+   divisions.push(new Divisions(k, height-divisionHeight/2,10, divisionHeight))
+ }
+ for (var j = 40; j <=width; j=j+50){
+  plinkos.push(new Plinko(j,75))}
+
+  for (var j = 15; j <=width-10 ; j=j+50){
+   plinkos.push(new Plinko(j,175))
+ }
+
+ for (var j = 40; j <=width; j=j+50){
+   plinkos.push(new Plinko(j,275))
+ }
+
+ for (var j = 15; j <=width-10; j=j+50){
+   plinkos.push(new Plinko(j,375))
+ }
+}
+
+function draw() {
+  background("white");  
+
+  Engine.update(engine);
+  for (var j = 0; j<divisions.length; j++){
+    divisions[j].display();
+  }
+
+  for (var k = 0; k<particles.length; k++){
+    plinkos[k].display();
+
+  }
+
+  if (frameCount% 60 === 0){
+    particles.push(new Particle(random(width/2 -10,width/2 +10),10,10))
+  
+}
+drawSprites();
+ground.display();
+}
